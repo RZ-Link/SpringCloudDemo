f3499fd1538b93eb4ebd368462dc416a1788c17c
piaorongzhen <prz_2022@qq.com>
2025-06-09 15:44:42 +0800

    修改mapper扫描方式

--- service/service-order/src/main/java/org/example/order/OrderApplication.java
+++ service/service-order/src/main/java/org/example/order/OrderApplication.java
@@ -1,6 +1,5 @@
 package org.example.order;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@MapperScan("org.example.order.mapper")
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
--- service/service-order/src/main/java/org/example/order/mapper/UserMapper.java
+++ service/service-order/src/main/java/org/example/order/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package org.example.order.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.example.order.entity.User;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
     User selectUsers(Long id);
 }
