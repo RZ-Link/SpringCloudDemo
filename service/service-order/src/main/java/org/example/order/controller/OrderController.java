3590a85000f504da69b2fcdb6fee3468b01fc980
piaorongzhen <prz_2022@qq.com>
2025-06-09 14:26:56 +0800

    新增数据库连接代码

--- service/service-order/src/main/java/org/example/order/controller/OrderController.java
+++ service/service-order/src/main/java/org/example/order/controller/OrderController.java
@@ -1,10 +1,14 @@
 package org.example.order.controller;
 
+import org.example.order.entity.User;
 import org.example.order.feign.ProductFeignClient;
+import org.example.order.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order/api/v1")
@@ -12,9 +16,16 @@ public class OrderController {
 
     @Autowired
     private ProductFeignClient productFeignClient;
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/testFeign")
     public String testFeign() {
         return productFeignClient.hello();
+    }
+
+    @GetMapping("/testMybatisPlus")
+    public List<User> testMybatisPlus() {
+        return userMapper.selectList(null);
     }
 }
---
+++ service/service-order/src/main/java/org/example/order/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package org.example.order.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.order.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
